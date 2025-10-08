a855b85328a0e9dfb11e8d81c2994305a98d15ca
xcCodebreaker <xccodebreaker619@gmail.com>
2025-10-08 12:54:42 +0600

    Bonfire Lit! - Go Fundamentals -> Iteration (Complete)

--- TestWithGo/iteration/repeat.go
+++ TestWithGo/iteration/repeat.go
@@ -1,11 +1,13 @@
 package iteration
 
+import "strings"
+
 const repeatCount = 5
 
 func Repeat(character string) string {
-	var repeated string
+	var repeated strings.Builder
 	for i := 0; i < repeatCount; i++ {
-		repeated += character
+		repeated.WriteString(character)
 	}
-	return repeated
+	return repeated.String()
 }
